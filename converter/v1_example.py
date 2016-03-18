0d57ef71626bafb06cdfc2e6c9315077cc314044
Mark Seaborn <mrs@mythic-beasts.com>
2016-03-17 21:51:53 -0700

    Remove commented-out code in v1_example

--- converter/v1_example.py
+++ converter/v1_example.py
@@ -24,22 +24,6 @@ def write_example(inputs_dir):
         write_file(os.path.join(inputs_dir, '%s.tab' % name),
                    header + data.replace(',', '\t'))
 
-    # Time definitions
-    #  * Investment periods, multi-year
-    #    -- new infrastructure is built at start of investment period?
-
-#     write_input('periods', """\
-# INVESTMENT_PERIOD,period_start,period_end
-# 2020,2017,2026
-# """)
-
-    # Financial parameters
-
-#     write_file(os.path.join(inputs_dir, 'financials.dat'), """\
-# param base_financial_year := 2015;
-# param interest_rate := .07;
-# param discount_rate := .05;
-# """)
     write_file(os.path.join(inputs_dir, 'misc_params.dat'), """\
 param scenario_id := 1;
 param num_years_per_period := 2;
@@ -76,15 +60,6 @@ year,carbon_emissions_relative_to_base
 la_start,la_end,transmission_line_id,existing_transfer_capacity_mw,transmission_length_km,transmission_efficiency,new_transmission_builds_allowed,dc_line
 """)
     # S11,S19,8290,5000,23.31,0.98506845,1,0
-
-#     write_input('timeseries', """\
-# TIMESERIES,ts_period,ts_duration_of_tp,ts_num_tps,ts_scale_to_period
-# 2020_all,2020,24,1,3652.5
-# """)
-#     write_input('timepoints', """\
-# timepoint_id,timestamp,timeseries
-# 1,2025011512,2020_all
-# """)
 
     # hours_in_sample = 24 hours/day * 365.25 days/year * 4
     #   occurrences/investment period = 35064.0
@@ -142,52 +117,6 @@ load_area,fuel,year,fuel_price
 S10,Gas,2014,0.043478261
 S10,Gas,2015,0.043478261
 """)
-
-
-#     write_input('loads', """\
-# LOAD_ZONE,TIMEPOINT,lz_demand_mw
-# South,1,6.2
-# """)
-
-    # Possible generators
-
-#     write_input('generator_info', """\
-# generation_technology,g_max_age,g_min_build_capacity,g_scheduled_outage_rate,g_forced_outage_rate,g_is_resource_limited,g_is_variable,g_is_baseload,g_is_flexible_baseload,g_is_cogen,g_competes_for_space,g_variable_o_m,g_energy_source,g_full_load_heat_rate
-# NG_CC,20,0,0.04,0.06,0,0,0,0,0,0,3.4131,NaturalGas,6.705
-# """)
-#     write_input('project_info', """\
-# PROJECT,proj_dbid,proj_gen_tech,proj_load_zone,proj_connect_cost_per_mw,proj_capacity_limit_mw,proj_variable_om
-# S-NG_CC,34,NG_CC,South,57566.6,.,3.4131
-# """)
-#     # Already-built projects.
-#     write_input('proj_existing_builds', """\
-# PROJECT,build_year,proj_existing_cap
-# S-NG_CC,2000,10
-# """)
-
-    # Fuel cost and CO2 intensity.  Cost can vary by load zone; CO2
-    # intensity does not.
-#     write_input('fuel_cost', """\
-# load_zone,fuel,period,fuel_cost
-# South,NaturalGas,2020,4
-# """)
-#     write_input('fuels', """\
-# fuel,co2_intensity,upstream_co2_intensity
-# NaturalGas,0.05306,0
-# """)
-
-#     write_input('non_fuel_energy_sources', """\
-# energy_source
-# """)
-#     write_input('proj_build_costs', """\
-# PROJECT,build_year,proj_overnight_cost,proj_fixed_om
-# S-NG_CC,2000,1143900,5868.3
-# """)
-#     write_input('variable_capacity_factors', """\
-# PROJECT,timepoint,proj_max_capacity_factor
-# """)
-
-#     write_file(os.path.join(inputs_dir, 'modules'), 'project.no_commit\n')
 
 
 def main():
